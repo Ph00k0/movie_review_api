32d4c9e7143268b1fecab8c5c0fbc27820481446
Karabo Phooko <Kmphooko@gmail.com>
2024-10-05 00:38:37 +0200

    Initial commit for Movie Review API

--- Downloads/MovieReviewAPI/MovieReviewAPI/reviews/models.py
+++ Downloads/MovieReviewAPI/MovieReviewAPI/reviews/models.py
@@ -7,5 +7,19 @@ class MovieReview(models.Model):
 
     def __str__(self):
         return self.title
+    
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
+    review_text = models.TextField()
+    rating = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 # Create your models here.
